cdafce6c8904faa695b6af3e8f4e6979fe2dd620
Statikman619 <martinez.michael02@gmail.com>
2021-05-26 17:38:03 -0700

    started work on my PORT

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const prompts = require("./lib/prompts");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "12345678",
+  database: "employee_tracker",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  prompts.firstQ();
+});
+
+exports.connection = connection;
