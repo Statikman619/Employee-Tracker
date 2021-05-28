0da9eddfa3cde96a4c219aa0623899d9eb6d6bb3
Statikman619 <martinez.michael02@gmail.com>
2021-05-27 18:11:44 -0700

    fixed my errors on schema and seeds

--- schema.sql
+++ schema.sql
@@ -3,25 +3,24 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-  id - INT PRIMARY KEY,
-  name - VARCHAR(30) to hold department name,
-  PRIMARY KEY(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30),
 );
 
 CREATE TABLE role (
-  id INT - INT PRIMARY KEY,
-  title -  VARCHAR(30) to hold role title,
-  salary -  DECIMAL to hold role salary,
-  department_id -  INT to hold reference to department role belongs to,
-  PRIMARY KEY(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INT - INT PRIMARY KEY,
-  first_name - VARCHAR(30) to hold employee first name,
-  last_name - VARCHAR(30) to hold employee last name,
-  role_id - INT to hold reference to role employee has,
-  is_manager - INT to hold reference to another employee that manages the employee,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
   manager_id INT 
-  PRIMARY KEY(id)
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
